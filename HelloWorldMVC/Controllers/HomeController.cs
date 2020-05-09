0edd3e92d048d2a7badf7c0c558e6a9181c67dc1
xernai <montcorona@gmail.com>
2020-05-09 15:31:21 -0500

    Add project files.

---
+++ HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            AppMessageModel obj = new AppMessageModel()
+            {
+                Message = "Hello World!"
+            };
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Index(AppMessageModel obj)
+        {
+            ViewBag.Message = "Message changed.";
+            return View(obj);
+        }
+    }
+}
